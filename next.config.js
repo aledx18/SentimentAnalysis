e169a7b5f23c05caf42f0c66c51e8cdbc3236e59
aledx18 <alediiaz18@gmail.com>
2023-02-16 15:51:13 -0300

    s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
+}
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://sentiment-analysis-liart.vercel.app/api/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
